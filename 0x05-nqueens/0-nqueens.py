80b4af17ef738ba5a8ac8e57882afc41e39b2867
NjengaC <victorcyrus01@gmail.com>
2024-07-03 21:17:57 +0300

    yield

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,92 +1,44 @@
 #!/usr/bin/python3
-
-"""Learn about the n queens problem and how to solve it"""
+""" N queens """
 import sys
 
-args = sys.argv
-if len(args) != 2:
+
+if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
     exit(1)
 
-n = args[1]
-try:
-    N = int(n)
-except ValueError:
+if not sys.argv[1].isdigit():
     print("N must be a number")
     exit(1)
 
-if N < 4:
+if int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
 
-matrix = [[0] * N for _ in range(N)]
+n = int(sys.argv[1])
 
 
-def valid_row(row):
-    """Validate a row that a queen can be placed"""
-    return sum(row) == 0
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
 
-def valid_column(index, matrix):
-    """Validate a column that a queen can be placed"""
-    for row in matrix:
-        if row[index] == 1:
-            return False
-    return True
-
-
-def valid_diagonal(mat, row, col):
-    """Validate a diagonal that a queen can be placed"""
-
-    for m in range(N):
-        if (mat[row][m] == 1) or (mat[m][col] == 1):
-            return False
-
-    for k in range(N):
-        for j in range(N):
-            if (k - j == row - col) or (k + j == row + col):
-                if mat[k][j] == 1:
-                    return False
-    return True
-
-
-def get_index(arr):
-    """Get index of position 1"""
+def solve(n):
+    """ solve """
+    k = []
     i = 0
-    while i < N:
-        if arr[i] == 1:
-            return i
-        i += 1
-    return -1
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
 
-def print_matrix(matrix):
-    """Print the positions of the n queen solution"""
-    index = []
-    for idx, row in enumerate(matrix):
-        index.append([idx, get_index(row)])
-    print(index)
-
-
-def generate_solutions(col, matrix, solutions):
-    """Generate solutions for the N queens problem"""
-    if col >= N:
-        solutions.append([row[:] for row in matrix])
-        return
-
-    for i in range(N):
-        if valid_column(col, matrix) and valid_row(
-                matrix[i]) and valid_diagonal(matrix, i, col):
-
-            matrix[i][col] = 1
-
-            generate_solutions(col + 1, matrix, solutions)
-            matrix[i][col] = 0
-
-
-if __name__ == '__main__':
-    solutions = []
-    generate_solutions(0, matrix, solutions)
-
-    for solution in solutions:
-        print_matrix(solution)
+solve(n)
